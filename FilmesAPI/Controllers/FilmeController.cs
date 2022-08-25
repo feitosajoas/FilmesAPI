757a7f6693551ee887ea7fc672b849475d975f6b
Joás Feitosa <joas.silva@dasa.com.br>
2022-08-25 11:36:09 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdcionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatório.")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O gênero não pode ultrapassar 30 caracteres.")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
